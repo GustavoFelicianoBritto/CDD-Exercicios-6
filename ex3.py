4e72af4769a0627ac082180fc25555f0be3186c7
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-04-29 15:41:53 -0300

    encontrar palavra

---
+++ ex3.py
@@ -0,0 +1,7 @@
+nomes=["gustavo","victor","sol","andresa","joão"]
+
+nome=input("Digite o nome em questão: ").lower()
+
+for i in range(len(nomes)):
+    if nome == nomes[i]:
+        print(f"Nome: {nome} encontrado na posição {i}")
