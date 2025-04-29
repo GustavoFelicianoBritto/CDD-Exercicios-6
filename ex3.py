047d4d3d5852af66966c6c922f78467e214ad485
Gustavo Feliciano <gustavofelicianoemail@gmail.com>
2025-04-29 20:40:32 -0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,10 +1,12 @@
 nomes=["gustavo","victor","sol","andresa","joão"]
-
+msg=""
 nome=input("Digite o nome em questão: ").lower()
 
 for i in range(len(nomes)):
     if nome == nomes[i]:
-        print(f"Nome: {nome} encontrado na posição {i}")
-    else:
-        print("Nome não encontrado")
+        msg=f"Nome {nome} encontrado na posição {i}"
         break
+    else:
+        msg="Nome não encontrado"
+
+print(msg)
