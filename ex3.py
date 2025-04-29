f08fc7e3b997074cbf18ac56f3d1feaddba9eddb
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-04-29 15:50:43 -0300

    encontrar palavra att

--- ex3.py
+++ ex3.py
@@ -5,3 +5,5 @@ nome=input("Digite o nome em questão: ").lower()
 for i in range(len(nomes)):
     if nome == nomes[i]:
         print(f"Nome: {nome} encontrado na posição {i}")
+    else:
+        print("Nome não encontrado")
